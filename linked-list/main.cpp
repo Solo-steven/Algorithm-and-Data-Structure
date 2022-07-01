26252959aefd14992a41bbaa202f22b5caa32009
Solo-steven <ssteven19@kimo.com>
2022-07-01 17:27:18 +0800

    Feat: Init Linked List Folder With C++

---
+++ linked-list/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int value;
+    Node *next;
+};
+
+struct Linked_List {
+    Node *head;
+};
+
+void push_node_to_list(Node * node, Linked_List *list) {
+
+} 
+
+void pop_node_from_list(Linked_List *list) {
+
+}
+
+void search_node_from_list(Node * node, Linked_List *list) {
+
+}
+
+int main()
+{
+    Linked_List *list = new Linked_List();
+    return 0;
+}
